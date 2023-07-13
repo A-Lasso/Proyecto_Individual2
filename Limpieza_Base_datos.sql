e2989a22fdc74e24ecdd9d5aa9fa1a6bb026f832
A-Lasso <lassoabril@gmail.com>
2023-07-13 17:52:42 -0300

    Preparando limpieza csv

---
+++ Limpieza_Base_datos.sql
@@ -0,0 +1,18 @@
+Drop database IF EXISTS Proyecto2;
+SET GLOBAL local_infile = 1;
+
+CREATE DATABASE IF NOT EXISTS Proyecto2;
+USE Proyecto2;
+
+CREATE TABLE IF NOT EXISTS `Penet_Inter_PROV_100HOG`(
+`Año`INT,
+`Trimestre`INT,
+`Provincia`VARCHAR(150),
+`Accesos por cada 100 hogares`VARCHAR(50)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+LOAD DATA INFILE 'D:\\Programacion\\DataScience_Henry\\Proyecto_Individual2\\csv_api\\Penet_Inter_PROV_100HOG.csv'
+INTO TABLE Penet_Inter_PROV_100HOG
+FIELDS TERMINATED BY ',' 
+LINES TERMINATED BY '\n' 
+IGNORE 1 LINES;
